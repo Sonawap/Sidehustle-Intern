4ef953ab5badc1073fa6383d606df3cc2df59314
Sonawap <paulsola79@gmail.com>
2020-10-14 07:28:38 -0700

    First PHP FILE

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// __________ First Major Task _________________
+
+echo "<p><b>Occupation:</b> Student</p>
+    <p><b>Gender:</b> Male</p>
+    <p><b>Sidehustle Username:</b> Sonawap</p>";
+
+///about me 
+echo "<h2>I am a Student of Federal University of Technology Akure, Ondo State</h2>";
+
+/// Sidehustle Internship username
+
+echo "<h3>Sonawap</h3>";
+
+echo "<p>
+        my name is Paul Sola Moses. I am an intern in the side hustle program on the category of Web development and Mobile app development
+    </p>";
+
+?>
+
